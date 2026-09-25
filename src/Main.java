8501de0b505f0ce77277faa6356bfef8bfd379d5
tyhoerlein <hoerletc@mail.uc.edu>
2026-09-25 12:42:11 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    double doubleSalesTax = .005;
+    double totalprice = 10;
+    double endprice;
+
+    endprice = totalprice * doubleSalesTax;
+    System.out.println ("doubleSlatesTax is " +endprice );
+
+}
